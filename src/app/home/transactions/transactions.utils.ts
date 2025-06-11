c027abff0551e9894447cf9bcda9044269ab4474
Jose López <jjsuarez8@hotmail.es>
2025-06-10 21:00:31 -0500

    Time in transactions

--- src/app/home/transactions/transactions.utils.ts
+++ src/app/home/transactions/transactions.utils.ts
@@ -1,4 +1,4 @@
-import { CurrencyPipe, NgClass } from '@angular/common';
+import { CurrencyPipe, DatePipe, NgClass } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import {
@@ -20,6 +20,7 @@ import {
 export const IMPORTS = [
   ReactiveFormsModule,
   CurrencyPipe,
+  DatePipe,
   NgClass,
 
   IonContent,
